9764a7e3b19540d14d237c7521e22d68b4a426dc
Chihyun Choi <goyang132@naver.com>
2015-11-21 08:21:41 +0900

    1

--- dialog/src/main/java/com/example/lycan/myapplication/MainActivity.java
+++ dialog/src/main/java/com/example/lycan/myapplication/MainActivity.java
@@ -1,29 +1,80 @@
 package com.example.lycan.myapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edt1, edt2, edt3, edt4;
+    Button btn1;
+    View dialogView, toastView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setTitle("사용자 정보 입력");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        edt1 = (EditText)findViewById(R.id.t1);
+        edt2 = (EditText)findViewById(R.id.t2);
+
+
+
+        btn1 = (Button)findViewById(R.id.btn);
+
+        btn1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                dialogView = (View)View.inflate(MainActivity.this, R.layout.dialog, null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+
+                edt3 = (EditText)dialogView.findViewById(R.id.dlgt1);
+                edt4 = (EditText)dialogView.findViewById(R.id.dlgt2);
+
+                dlg.setTitle("사용자 정보 입력");
+                dlg.setIcon(R.mipmap.ic_launcher);
+                dlg.setView(dialogView);
+
+                edt3.setText(edt1.getText().toString());
+                edt4.setText(edt2.getText().toString());
+
+                dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        edt1.setText(edt3.getText().toString());
+                        edt2.setText(edt4.getText().toString());
+                    }
+                });
+                dlg.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = new Toast(MainActivity.this);
+                        toastView = (View)View.inflate(MainActivity.this, R.layout.toast, null);
+                        toast.setView(toastView);
+                        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+                        int xOffset = (int)(Math.random()* display.getWidth());
+                        int yOffset = (int)(Math.random()* display.getHeight());
+
+                        toast.setGravity(Gravity.TOP | Gravity.LEFT, xOffset, yOffset);
+                        toast.show();
+                    }
+                });
+                dlg.show();
             }
         });
     }
